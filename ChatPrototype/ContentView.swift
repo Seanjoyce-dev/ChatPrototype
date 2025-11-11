883345e6dd1b65f1d99b70563e236ae6c632632e
Sean Joyce <seanthomasjoyce@icloud.com>
2025-11-11 20:55:02 +0000

    Created a view for chat bubble

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -10,20 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 25) {
-            Text("Knock, Knock!")
-                .padding()
-                .background(.yellow, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: 8)
-            Text("Who's There")
-                .padding()
-                .background(.teal, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: 8)
-            Text("Bob")
-                .padding()
-                .background(.yellow, in: RoundedRectangle(cornerRadius: 8))
-                .shadow(radius: 8)
+            ChatBubble(message: "Knock, Knock!", isSender: true)
+            ChatBubble(message: "Who's There?", isSender: false)
+            ChatBubble(message: "Bob", isSender: true)
         }
         .padding()
+    }
+}
+
+struct ChatBubble: View {
+    let message: String
+    let isSender: Bool
+
+    var bubbleColor: Color {
+        isSender ? .yellow : .teal
+    }
+
+    var body: some View {
+        Text(message)
+            .padding()
+            .background(bubbleColor, in: RoundedRectangle(cornerRadius: 8))
+            .shadow(radius: 8)
     }
 }
 
